ca595b862f1823d550df9fdaf7f94d26b0a57a73
Shadowbreaker GD <78216950+ShadowbreakerGD@users.noreply.github.com>
2021-04-12 15:40:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-window.scrollTo(0,document.querySelector(".scrollingContainer").scrollHeight);
+var mybutton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
